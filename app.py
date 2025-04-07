621f5d5ef7417708763bb746e0d1fba0f93c881c
Frankbanna <Chaloemphonbannarak@gmail.com>
2025-04-07 15:24:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -145,5 +145,9 @@ def create_image(lottery_type):
     image.save(output_path)
     return output_path
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__": Edit By  Frank
+   # app.run(debug=True)
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
