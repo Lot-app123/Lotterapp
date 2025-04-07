79eeee4833779bcacfbfc02032c9e7b3140a649b
Frankbanna <Chaloemphonbannarak@gmail.com>
2025-04-07 16:05:44 +0700

    Deploy Flask on Render

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, send_file, redirect, url_for, session
-from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin, current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin, current_user 
 from datetime import datetime
 import random
 import os
@@ -145,9 +145,10 @@ def create_image(lottery_type):
     image.save(output_path)
     return output_path
 
-#if __name__ == "__main__": Edit By  Frank
-   # app.run(debug=True)
-    if __name__ == "__main__":
+
+#if __name__ == "__main__":Edit By Frank 
+ #   app.run(debug=True)
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
